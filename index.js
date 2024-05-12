148423b37aa9c16a5614c0048140d954cdfe05b9
arbinzaman <arbinzmn@gmail.com>
2024-05-13 00:59:02 +0600

    Project Initialized

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const mysql = require("mysql");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+async function main() {
+  try {
+    const db = mysql.createConnection({
+      host: "localhost",
+      user: "root",
+      password: "",
+      database: "event_guchai",
+    });
+
+    app.get("/", (req, res) => {
+      return res.json("Welcome to the event_guchai API");
+    });
+
+    app.listen(3001, () => {
+      console.log("running on port 3001");
+    });
+  } catch (error) {
+    console.error("Error connecting to the database: " + error);
+  }
+}
+
+main();
