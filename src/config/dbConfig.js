1abd80cd7c4c333699563b6cb9df7b2435e6d9a0
arbinzaman <arbinzmn@gmail.com>
2024-05-19 04:10:09 +0600

    Added Vercel.json config file and data base hossted

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -2,10 +2,16 @@ const express = require("express");
 const mysql = require("mysql");
 
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "event_guchai",
+    host: "bkxlrwtmemh81aeknnw9-mysql.services.clever-cloud.com",
+    user: "urwwmksjexojxkn0",
+    password: "nw3aZdz7yG2QBKzBLxcu",
+    database: "bkxlrwtmemh81aeknnw9",
   });
+// const db = mysql.createConnection({
+//     host: "localhost",
+//     user: "root",
+//     password: "",
+//     database: "event_guchai",
+//   });
 
   module.exports = db;
