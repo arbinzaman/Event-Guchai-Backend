82245389e841c0d35a4eb1fe5687ae4ecd3b4d70
arbinzaman <arbinzmn@gmail.com>
2024-05-19 23:49:28 +0600

    Added new vercelURL to cors

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ const customPackageRoute = require("./src/routes/CustomPackage");
 app.use(
   cors({
     origin: "http://localhost:5173",
+    origin: "https://event-guchai.vercel.app",
   })
 );
 
